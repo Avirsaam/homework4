d1c68e47ecb3d66293215ac05478b2eef9324d12
AvirSaam <den.demchenko@gmail.com>
2023-02-01 17:43:58 +0300

    task29 function implemented

--- task29/Program.cs
+++ task29/Program.cs
@@ -4,18 +4,39 @@
 6, 1, 33 -> [6, 1, 33]
 */
 
+
+void fillArrayWithRandomNumbers(int size, int min, int max, out int[] resultArray){
+    
+    resultArray = default;
+    
+    int[] result = new int[size];
+
+    Random rnd = new Random();
+    int [] myArray = new int[size];
+    
+
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = rnd.Next(min, max);
+    }
+
+    resultArray = result;
+}
+
+
 const int arraySize = 8;
 const int minRandomNumber = 0;
 const int maxRandomNumber = 100;
 
-Random rnd = new Random();
-int [] myArray = new int[arraySize];
+int[] resultArray;
+
+fillArrayWithRandomNumbers(arraySize, minRandomNumber, maxRandomNumber, out resultArray);
 
 Console.Write("[");
 for (int i = 0; i < arraySize; i++)
 {
-    myArray[i] = rnd.Next(minRandomNumber, maxRandomNumber);    
-    Console.Write(myArray[i]);
+    
+    Console.Write(resultArray[i]);
     
     if (arraySize - i > 1)
     {
